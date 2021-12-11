aa6accf96a35f16fbf2feed66b466ab0585c2212
Serverguyken <serverguyken@gmail.com>
2021-12-11 14:23:51 -0500

    added extension images

--- client/out/helpers/fileLanguage.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const fileLanguage = "alden";
-exports.default = fileLanguage;
-//# sourceMappingURL=fileLanguage.js.map
